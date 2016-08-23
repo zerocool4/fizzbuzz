ff477549698108a99bbdaa7a03903da4da5f9bb6
Velo Burn <veloburn@hotmail.com>
2016-08-23 01:36:04 -0700

    -First solution -New fizzbuzz function -Change variable name

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-if __name__ == '__main__':
-	main() # Testing
+def fizzbuzz():
+	for num in xrange(1, 100):
+		if num % 3 == 0 and num % 5 == 0:
+			print "FizzBuzz"
+		elif num % 5 == 0:
+			print "Buzz"
+		elif num % 3 == 0:
+			print "Fizz"
+		else:
+			print num
 
-def function():
-	pass
-	main() 
+
+def main():
+	fizzbuzz()
+
+
+if __name__ == '__main__':
+	main()
